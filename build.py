c509fbe1500b39c439e1d717e49e5590d053b544
Shubhankar Barman <99989566+shubhankarbarmangithub@users.noreply.github.com>
2022-10-09 16:31:37 +0530

    Update build.py

--- build.py
+++ build.py
@@ -3,7 +3,7 @@ import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
 
-df=pd.read_csv('deploy_df')
+df=pd.read_csv('deploy_df.csv')
 
 df.drop('Unnamed: 0',axis=1,inplace=True)
 
